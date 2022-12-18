658d17692c8ff9bd47ed8d2e69ccf8aea2e7e18b
Jean Sossmeier <jean.sossmeier@datassential.com>
2022-12-18 15:56:18 -0300

    feature: create where directive initial implementation

--- src/main/java/com/joutvhu/dynamic/commons/handlebars/helpers/CustomDirectiveHelpers.java
+++ src/main/java/com/joutvhu/dynamic/commons/handlebars/helpers/CustomDirectiveHelpers.java
@@ -10,6 +10,7 @@ import lombok.SneakyThrows;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.apache.commons.lang3.Validate.notNull;
 
@@ -21,7 +22,46 @@ public enum CustomDirectiveHelpers implements Helper<Object> {
         @SneakyThrows
         protected CharSequence safeApply(final Object value, final Options options) {
             CharSequence content = options.fn();
-            //TODO: trim symbols
+            return normalize(content.toString());
+        }
+
+        private final TrimDirective.TrimSymbol symbols = new TrimDirective.TrimSymbol(
+                "where", getOverrides(true, "and", "or"),
+                null, getOverrides(false, "and", "or"));
+
+        private final List<String> getOverrides(boolean prefix, String... overrides) {
+            List<String> result = new ArrayList<>();
+            for (String o : overrides) {
+                result.add(prefix ? o + " " : " " + o);
+                result.add(prefix ? o + "\n" : "\n" + o);
+                result.add(prefix ? o + "\t" : "\t" + o);
+            }
+            return result;
+        }
+
+        private String escapeRegular(String regex) {
+            return Pattern.compile("([-/\\\\^$*+?.()|\\[\\]{}])").matcher(regex).replaceAll("\\\\$1");
+        }
+
+        public String normalize(String content) throws IOException {
+            for (String prefix : symbols.getPrefixOverrides())
+                content = Pattern.compile("^[ \\t\\n]*" + escapeRegular(prefix), Pattern.CASE_INSENSITIVE)
+                        .matcher(content)
+                        .replaceAll("");
+
+            for (String suffix : symbols.getSuffixOverrides())
+                content = Pattern.compile(escapeRegular(suffix) + "[ \\t\\n]*$", Pattern.CASE_INSENSITIVE)
+                        .matcher(content)
+                        .replaceAll("");
+
+            content = content.trim();
+            if (!content.isEmpty()) {
+                if (symbols.getPrefix() != null)
+                    content = symbols.getPrefix() + " " + content;
+                if (symbols.getSuffix() != null)
+                    content = content + " " + symbols.getSuffix();
+            }
+            content = " " + content + " ";
             return content;
         }
     };
